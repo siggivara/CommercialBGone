a1ea141ae770c81f703064ea211f1c28cfb87032
siggivara <sigvefast88@yahoo.no>
2014-02-02 00:45:12 +0100

    removed unused references

--- CommercialBGone/Program.cs
+++ CommercialBGone/Program.cs
@@ -8,7 +8,6 @@
             const string sourceImagePath = @"C:\source\images\tv2_2_transparent.png";
 
             var match = TemplateMatching.TemplateMatching.TemplateIsInImage(templatePath, sourceImagePath);
-
         }
     }
 }
