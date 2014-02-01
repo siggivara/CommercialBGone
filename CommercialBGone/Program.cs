521128b1f195789f879c942cd2792ea022645009
siggivara <sigvefast88@yahoo.no>
2014-02-02 00:39:17 +0100

    Initial commit

---
+++ CommercialBGone/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CommercialBGone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string templatePath = @"C:\source\images\tv2_2_transparent.png";
+            const string sourceImagePath = @"C:\source\images\tv2_2_transparent.png";
+
+            var match = TemplateMatching.TemplateMatching.TemplateIsInImage(templatePath, sourceImagePath);
+        }
+    }
+}
---
+++ ImageProcessing/TemplateMatching.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace TemplateMatching
+{
+    public class TemplateMatching
+    {
+        public static bool TemplateIsInImage(string templateImagePath, string sourceImagePath)
+        {
+            var templateImage = new Image<Bgr, Byte>(templateImagePath);
+            var sourceImage = new Image<Bgr, Byte>(sourceImagePath);
+
+            Image<Gray, float> resultImage = sourceImage.MatchTemplate(templateImage,
+                Emgu.CV.CvEnum.TM_TYPE.CV_TM_CCOEFF_NORMED);
+
+            float[, ,] matches = resultImage.Data;
+            for (int x = 0; x < matches.GetLength(1); x++)
+            {
+                for (int y = 0; y < matches.GetLength(0); y++)
+                {
+                    double matchScore = matches[y, x, 0];
+                    if (matchScore > 0.75)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
